8eb614fbd241f72a927d95ac967fc6f443b295dd
Lauragg <lauragogar@correo.ugr.es>
2018-04-26 15:01:38 +0200

    Copia de todo el trabajo actual

---
+++ Practica2/Comportamientos_Jugador/jugador.cpp
@@ -0,0 +1,164 @@
+#include "../Comportamientos_Jugador/jugador.hpp"
+#include "motorlib/util.h"
+
+#include <iostream>
+
+void ComportamientoJugador::PintaPlan(list<Action> plan) {
+	auto it = plan.begin();
+	while (it!=plan.end()){
+		if (*it == actFORWARD){
+			cout << "A ";
+		}
+		else if (*it == actTURN_R){
+			cout << "D ";
+		}
+		else if (*it == actTURN_L){
+			cout << "I ";
+		}
+		else {
+			cout << "- ";
+		}
+		it++;
+	}
+	cout << endl;
+}
+
+bool ComportamientoJugador::pathFinding(const estado &origen, const estado &destino, list<Action> &plan) {
+	list<celda> abierto;
+	list<celda> cerrado;
+	bool finalizado=false;
+	celda actual, dest=celda(NULL,destino.fila,destino.columna,destino.orientacion,destino.fila,destino.columna),
+				orig=celda(NULL,origen.fila,origen.columna,origen.orientacion,destino.fila,destino.columna);
+
+	//Paso 0
+	abierto.push_front(orig);
+
+	while(!finalizado){
+		//Paso 1
+		actual=abierto.front();
+		cerrado.push_back(actual);
+		abierto.pop_front();
+		//Paso 2
+		if(actual.accesible(mapaResultado)){
+			list<celda> adyacentes=actual.getAdyacentes();
+			for(list<celda>::iterator iter=adyacentes.begin(); iter!=adyacentes.end() && !finalizado ; ++iter){
+				//Vemos si la celda escogida es el destino, en cuyo caso finalizamos.
+				if((*iter)==dest){
+					actual=*iter;
+					finalizado=true;
+						cout << "He llegado 2"<<endl;
+				}
+				else if(iter->accesible(mapaResultado) && !celdaCerrada(*iter,cerrado)){
+						//Comprobamos si iter pertenece a la lista de abiertos.
+						bool pertenece=false;
+						list<celda>::iterator iterAb=abierto.begin();
+						while(!pertenece && iterAb!=abierto.end()){
+							if((*iter)==(*iterAb)){
+								pertenece=true;
+								//Si la g es mejor por el nuevo camino, entonces sustituimos.
+								if(iter->menorOrigen(*iterAb)){
+									iterAb=abierto.erase(iterAb);
+									iterAb=abierto.insert(iterAb,*iter);
+								}
+							}
+							++iterAb;
+						}
+						//Si no pertenece a la lista de abiertos, la añadimos.
+						//Se recuerda que esto se ejecuta sólo si es accesible y no pertenece
+						// a la lista de cerrados-
+						if(!pertenece)
+							abierto.push_back(*iter);
+				}
+			}
+		}
+		//Paso 3: Reordenamos la lista de abiertos.
+		abierto.sort();
+	}
+		cout << "He llegado 3"<<endl;
+	calcularPlan(orig,actual,plan);
+		cout << "He llegado 4"<<endl;
+
+	PintaPlan(plan);
+
+	return true;
+}
+
+Action ComportamientoJugador::think(Sensores sensores) {
+	Action accion=actIDLE;
+	if(sensores.mensajeC!=-1){
+		fil=sensores.mensajeF;
+		col=sensores.mensajeC;
+	}
+
+	if(destino.fila!=sensores.destinoF || destino.columna!=sensores.destinoC){
+		destino.fila=sensores.destinoF;
+		destino.columna=sensores.destinoC;
+		cout << "He llegado 1" <<endl;
+		pathFinding(estado(fil,col,brujula),destino,plan);
+	}
+
+	accion=plan.front();
+	plan.pop_front();
+
+  return accion;
+}
+
+
+
+bool ComportamientoJugador::celdaCerrada(const celda & celd, const list<celda> &cerrado){
+	bool cerrada=false;
+	list<celda>::const_iterator iter=cerrado.begin();
+	while(!cerrada && iter!=cerrado.end()){
+		if(celd==(*iter)) cerrada=true;
+		++iter;
+	}
+
+	return cerrada;
+}
+
+void ComportamientoJugador::calcularPlan(const celda & origen, const celda & destino, list<Action> &plan){
+	list<celda> ruta;
+	celda actual=destino;
+	int pos, giros;
+		cout << "He llegado 2-1"<<endl;
+	//Inicializamos ruta, insertándole todas las celdas en orden de lectura.
+	while(!(actual==origen)){
+		ruta.push_front(actual);
+		actual=*actual.getPadre();
+	}
+		cout << "He llegado 2-2"<<endl;
+	ruta.push_front(origen);
+
+
+	//Comienzo la búsqueda.
+	list<celda>:: iterator it=ruta.begin();
+		cout << "He llegado 2-3"<<endl;
+	while(it!=ruta.end()){
+		//Vamos a recorrer las celdas adyacentes a la celda actual.
+		actual=*it;
+		list<celda>::iterator iter=actual.getAdyacentes().begin();
+		//Con la celda siguiente.
+		++it;
+		//Son exactamente 4 adyacentes siempre.
+		for(int i=0; i<4; i++){
+			if((*iter)==(*it)) pos=i;
+			++iter;
+		}
+		//Así sabemos cuántos giros tenemos que dar y en qué dirección.
+		giros=actual.getOrientacion()-pos;
+		if(giros==1 || giros == -3)
+			plan.push_back(actTURN_L);
+		else if(giros==0) ;
+		else{
+			plan.push_back(actTURN_R);
+			if(giros==-2 || giros ==2)
+				plan.push_back(actTURN_R);
+		}
+		plan.push_back(actFORWARD);
+		it->setOrientacion(pos);
+	}
+}
+
+int ComportamientoJugador::interact(Action accion, int valor){
+  return false;
+}
---
+++ Practica2/Comportamientos_Jugador/jugador.hpp
@@ -0,0 +1,197 @@
+#ifndef COMPORTAMIENTOJUGADOR_H
+#define COMPORTAMIENTOJUGADOR_H
+
+#include "comportamientos/comportamiento.hpp"
+
+#include <list>
+#include <climits>
+#include <cmath>
+#include <iostream>
+
+struct estado {
+  int fila;
+  int columna;
+  int orientacion;
+
+  estado(){}
+  estado(int fil, int col, int orient) : fila(fil), columna(col), orientacion(orient){}
+  estado(const estado & est) : fila(est.fila), columna(est.columna), orientacion(est.orientacion){}
+};
+
+/*
+  clase celda, utilizada para nuestro algoritmo de búsqueda
+*/
+class celda{
+ private:
+  celda * padre;
+  list <celda> adyacentes;
+  int distanciaOrigen=0;
+  int dest_fila,dest_columna;
+  estado pos;
+
+
+  void CalcAdy(){
+    if(adyacentes.empty()){
+      int pos_fil;
+      int pos_col;
+      pos_fil=pos.fila-1; pos_col=pos.columna;
+      if(pos_fil >= 0 && pos_fil <= 100 && pos_col >=0 && pos_col <= 100)
+        adyacentes.emplace_back(this,pos_fil,pos_col,0,dest_fila,dest_columna);
+
+      pos_fil=pos.fila;pos_col=pos.columna+1;
+      if(pos_fil >= 0 && pos_fil <= 100 && pos_col >=0 && pos_col <= 100)
+        adyacentes.emplace_back(this,pos_fil,pos_col,1,dest_fila,dest_columna);
+
+      pos_fil=pos.fila+1;pos_col=pos.columna;
+      if(pos_fil >= 0 && pos_fil <= 100 && pos_col >=0 && pos_col <= 100)
+        adyacentes.emplace_back(this,pos_fil,pos_col,2,dest_fila,dest_columna);
+
+      pos_fil=pos.fila;pos_col=pos.columna-1;
+      if(pos_fil >= 0 && pos_fil <= 100 && pos_col >=0 && pos_col <= 100)
+        adyacentes.emplace_back(this,pos_fil,pos_col,3,dest_fila,dest_columna);
+    }
+
+  }
+
+  int CalcDistHeur() const{
+      return abs(pos.fila-dest_fila)+abs(pos.columna-dest_columna);
+  }
+
+  void CalcDistOrig(){
+    if(padre!= NULL)
+      distanciaOrigen=padre->distanciaOrigen+10;
+  }
+
+  celda(celda * papa){
+    padre=papa;
+  }
+
+ public:
+  celda(){}
+
+/**
+  celda(celda * papa, list <celda> adyc, int fils, int cols, int bruj, int dfils, int dcols) :
+    padre(papa), adyacentes(adyc), pos(fils,cols,bruj){
+      dest_fila=fils;
+      dest_colummna=dcols;
+    }
+*/
+
+  //celda(celda * papa, list <celda> adyc, estado est):
+    //padre(papa), adyacentes(adyc), pos(est){}
+
+  celda(const celda & copia) :
+    celda(copia.padre,copia.pos.fila,copia.pos.columna,copia.pos.orientacion,copia.dest_fila,copia.dest_columna){
+    distanciaOrigen=copia.distanciaOrigen;
+  }
+  celda(celda * papa, int fils, int cols, int bruj,int dfils, int dcols) :
+    padre(papa), pos(fils,cols,bruj){
+      dest_fila=fils;
+      dest_columna=dcols;
+      CalcDistOrig();
+    }
+
+//  celda(celda * papa, estado est) :
+  //  celda(papa,est.fila,est.columna,est.orientacion){}
+
+  list<celda> getAdyacentes() {
+    if(adyacentes.empty())
+      CalcAdy();
+    return adyacentes;}
+
+  celda * getPadre(){
+    return padre;}
+
+  int getOrientacion(){return pos.orientacion;}
+
+  void setOrientacion(int orienta){pos.orientacion=orienta;}
+
+  bool operator ==(const celda & b) const{
+    return pos.fila==b.pos.fila && pos.columna==b.pos.columna;
+  }
+
+/*  celda & operator=(const celda & b){
+    if(this!= &b){
+      padre=b.padre;
+      pos.fila=b.pos.fila;
+      pos.columna=b.pos.columna;
+      pos.orientacion=b.pos.orientacion;
+      adyacentes=b.adyacentes;
+      distanciaOrigen=b.distanciaOrigen;
+      dest_fila=b.dest_fila;
+      dest_columna=b.dest_columna;
+    }
+  }*/
+
+/*
+  Este método nos devolverá true si tenemos una menor distancia al Origen o no en sentido estricto.
+*/
+  bool menorOrigen(const celda & b) const{
+    return distanciaOrigen < b.distanciaOrigen;
+  }
+
+/*
+  Este método nos dirá si nuestra celda es MEJOR o no estrictamente, en el sentido de que una celda es mejor
+  cuando MENOR sea valorCelda.
+*/
+  bool operator <(const celda & b) const{
+    return valorCelda() < b.valorCelda();
+  }
+
+/*
+  El valor de una celda será la suma de distanciaOrigen y distanciaHeuristica.
+*/
+  int valorCelda()const {return distanciaOrigen+CalcDistHeur();}
+
+  /*
+  Nos indica si la celda es accesible o no.
+  */
+  bool accesible(vector<vector<unsigned char> > mapaResultado){
+    char tipo=mapaResultado[pos.fila][pos.columna];
+    if(tipo=='T' || tipo=='S' || tipo=='K' || tipo=='?')
+      return true;
+    else return false;
+  }
+};
+
+class ComportamientoJugador : public Comportamiento {
+  public:
+    ComportamientoJugador(unsigned int size) : Comportamiento(size) {
+      // Inicializar Variables de Estado
+      fil = col = 99;
+      brujula = 0; // 0: Norte, 1:Este, 2:Sur, 3:Oeste
+      destino.fila = -1;
+      destino.columna = -1;
+      destino.orientacion = -1;
+    }
+    ComportamientoJugador(std::vector< std::vector< unsigned char> > mapaR) : Comportamiento(mapaR) {
+      // Inicializar Variables de Estado
+      fil = col = 99;
+      brujula = 0; // 0: Norte, 1:Este, 2:Sur, 3:Oeste
+      destino.fila = -1;
+      destino.columna = -1;
+      destino.orientacion = -1;
+    }
+    ComportamientoJugador(const ComportamientoJugador & comport) : Comportamiento(comport){}
+    ~ComportamientoJugador(){}
+
+    Action think(Sensores sensores);
+    int interact(Action accion, int valor);
+    ComportamientoJugador * clone(){return new ComportamientoJugador(*this);}
+
+  private:
+    // Declarar Variables de Estado
+    int fil, col, brujula;
+    estado destino;
+    list<Action> plan;
+    //list<celda> cerrado;
+
+
+    bool pathFinding(const estado &origen, const estado &destino, list<Action> &plan);
+    void PintaPlan(list<Action> plan);
+    bool celdaCerrada(const celda & celd, const list<celda> &cerrado);
+    void calcularPlan(const celda & origen, const celda & destino, list<Action> &plan);
+
+};
+
+#endif
